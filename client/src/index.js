3b2d322a927bce2cb7996a182fb04c426d94522a
sammiullah <codetorule@gmail.com>
2021-07-29 13:26:04 +0500

    assets in public folder

--- client/src/index.js
+++ client/src/index.js
@@ -5,45 +5,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import '../src/assets/css/bootstrap.min.css'
-import $ from "jquery";
-
-// theme
-import "../src/assets/plugins/simplebar/css/simplebar.css"
-import "../src/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css"
-import "../src/assets/plugins/metismenu/css/metisMenu.min.css" 
-
-
-
-
-import "../src/assets/css/pace.min.css" 
-
-
-import "../src/assets/css/bootstrap.min.css"
-// import "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
-import "../src/assets/css/app.css" 
-import "../src/assets/css/icons.css"
-
-import "../src/assets/css/dark-theme.css" 
-import "../src/assets/css/semi-dark.css" 
-import "../src/assets/css/header-colors.css" 
-
-/// end theme
-// import "../src/assets/js/pace.min.js"
-
-
-// import "../src/assets/js/bootstrap.bundle.min.js"
-// import "../src/assets/js/jquery.min.js"
-// import "../src/assets/plugins/simplebar/js/simplebar.min.js"
-// import "../src/assets/plugins/metismenu/js/metisMenu.min.js"
-// import "../src/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"
-
-// import "../src/assets/js/app.js"
-
-
-// import "../src/assets/js/bootstrap.bundle.min.js"
-
-
 
 
 ReactDOM.render(
