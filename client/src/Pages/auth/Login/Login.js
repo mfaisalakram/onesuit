fabcc49c447516e5c3e8042a2462c999437f265f
sammiullah <codetorule@gmail.com>
2021-07-31 01:25:07 +0500

    Login working

--- client/src/Pages/auth/Login/Login.js
+++ client/src/Pages/auth/Login/Login.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 // form validation
-import { useForm } from 'react-hook-form';
-import * as Yup from 'yup';
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
   const validationSchema = Yup.object().shape({
-    email: Yup.object().shape({
-      value: Yup.string().required('Email is required'),
-    }),
-    password: Yup.string().required('Call Name is required'),
+    email: Yup.string().required("Email is required"),
+    password: Yup.string().required("Call Name is required"),
   });
 
   const {
+    register,
     handleSubmit,
     reset,
     formState: { errors },
@@ -25,30 +25,19 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const { email, password } = formData;
-
-  const onChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const qwerty = (data) => {
+    console.log(data);
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const getData = async () => {
+  const getData = async (data) => {
     const responce = await axios
-      .post('https://test-node-samiullah.herokuapp.com/accounts/login', {
-        username: formData.email,
-        password: formData.password,
+      .post("https://test-node-samiullah.herokuapp.com/accounts/login", {
+        username: data.email,
+        password: data.password,
       })
       .then((result) => {
-        if (result.data.status == 'success') {
-          navigate('/account');
+        if (result.data.status == "success") {
+          navigate("/account");
         } else {
           console.log(result.status);
           console.log(result.data.status);
@@ -63,7 +52,7 @@ const Login = () => {
 
   return (
     <div
-      style={{ padding: '30px 100px 100px 100px' }}
+      style={{ padding: "30px 100px 100px 100px" }}
       className="row row-cols-1 row-cols-lg-2 row-cols-xl-3"
     >
       <div className="col mx-auto">
@@ -73,13 +62,13 @@ const Login = () => {
               <div className="text-center">
                 <h3 className="">Sign in</h3>
                 <p>
-                  Don't have an account yet?{' '}
+                  Don't have an account yet?{" "}
                   <Link to="/register">Sign up here</Link>
                 </p>
               </div>
               <div className="d-grid">
                 <a className="btn my-4 shadow-sm btn-white" href="javascript:;">
-                  {' '}
+                  {" "}
                   <span className="d-flex justify-content-center align-items-center">
                     <img
                       className="me-2"
@@ -89,20 +78,20 @@ const Login = () => {
                     />
                     <span>Sign in with Google</span>
                   </span>
-                </a>{' '}
+                </a>{" "}
                 <a href="javascript:;" className="btn btn-facebook">
                   <i className="bx bxl-facebook"></i>Sign in with Facebook
                 </a>
               </div>
               <div className="login-separater text-center mb-4">
-                {' '}
+                {" "}
                 <span>OR SIGN IN WITH EMAIL</span>
                 <hr />
               </div>
               <div className="form-body">
                 <form
                   className="row g-3"
-                  onSubmit={handleSubmit(onSubmit)}
+                  onSubmit={handleSubmit((data) => getData(data))}
                   onReset={reset}
                   // onSubmit={(e) => onSubmit(e)}
                 >
@@ -110,15 +99,8 @@ const Login = () => {
                     <label for="inputEmailAddress" className="form-label">
                       Email Address
                     </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="inputEmailAddress"
-                      placeholder="Email Address"
-                      onChange={(e) => onChange(e)}
-                      name="email"
-                      value={email}
-                    />
+                    <input className="form-control" {...register("email")} />
+                    <p>{errors.email?.message}</p>
                   </div>
                   <div className="col-12">
                     <label for="inputChoosePassword" className="form-label">
@@ -128,12 +110,9 @@ const Login = () => {
                       <input
                         type="password"
                         className="form-control border-end-0"
-                        id="inputChoosePassword"
-                        placeholder="Enter Password"
-                        onChange={(e) => onChange(e)}
-                        value={password}
                         name="password"
-                      />{' '}
+                        {...register("password")}
+                      />{" "}
                       <a
                         href="javascript:;"
                         className="input-group-text bg-transparent"
@@ -159,7 +138,7 @@ const Login = () => {
                     </div>
                   </div>
                   <div className="col-md-6 text-end">
-                    {' '}
+                    {" "}
                     <a href="authentication-forgot-password.html">
                       Forgot Password ?
                     </a>
@@ -167,8 +146,8 @@ const Login = () => {
                   <div className="col-12">
                     <div className="d-grid">
                       <button
-                        onClick={getData}
-                        type="button"
+                        // onClick={getData}
+                        type="submit"
                         className="btn btn-primary"
                       >
                         <i className="bx bxs-lock-open"></i>
