f9e913117aa8d5c9ce195f95b24873e568a04ed7
Faisal <faisalakram1133@gmail.com>
2021-08-02 17:14:00 +0500

    message

--- client/src/Pages/Ticket/IncidentManagement.js
+++ client/src/Pages/Ticket/IncidentManagement.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const IncidentManagement = () => {
-    return (
-        <div>
-            <Outlet /> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
 
-export default IncidentManagement
+export default IncidentManagement;
